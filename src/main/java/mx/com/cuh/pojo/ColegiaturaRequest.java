eae0bf0de682161fa39d9494a713fa0603da7c6e
Leono Morales R <leonomreyes18@gmail.com>
2023-10-20 07:06:32 -0600

    Colegiatura Request

--- src/main/java/mx/com/cuh/pojo/ColegiaturaRequest.java
+++ src/main/java/mx/com/cuh/pojo/ColegiaturaRequest.java
@@ -1,21 +1,27 @@
 package mx.com.cuh.pojo;
 
-public class Alumnos {
-	private String matricula;
-	private String nombre;
-	
-	public String getMatricula() {
-		return matricula;
-	}
-	public void setMatricula(String matricula) {
-		this.matricula = matricula;
-	}
-	public String getNombre() {
-		return nombre;
-	}
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-	
-	
-} 
+import java.time.LocalDate;
+
+public class ColegiaturaRequest {
+private Long alumnoId;
+public Long getAlumnoId() {
+	return alumnoId;
+}
+public void setAlumnoId(Long alumnoId) {
+	this.alumnoId = alumnoId;
+}
+public Double getMonto() {
+	return monto;
+}
+public void setMonto(Double monto) {
+	this.monto = monto;
+}
+public LocalDate getFechaPago() {
+	return fechaPago;
+}
+public void setFechaPago(LocalDate fechaPago) {
+	this.fechaPago = fechaPago;
+}
+private Double monto;
+private LocalDate fechaPago;
+}
