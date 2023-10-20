c4f9b107365ca6f414ccb79bf1117aa504aa4c5c
Leono Morales R <leonomreyes18@gmail.com>
2023-10-20 09:17:57 -0600

    AlumnoRequest

---
+++ src/main/java/mx/com/cuh/pojo/AlumnoRequest.java
@@ -0,0 +1,21 @@
+package mx.com.cuh.pojo;
+
+public class AlumnoRequest {
+	private String matricula;
+	private String nombre;
+	
+	public String getMatricula() {
+		return matricula;
+	}
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	
+} 
